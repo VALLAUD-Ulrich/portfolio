4fc84a1c65b93441d3cf5cde51fd905911626a84
VALLAUD-Ulrich <ulrichvallaud@hotmail.fr>
2024-02-02 10:57:51 +0100

    manque le fonctionnement du formulaire de contacte

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Burger from "./components/burger";
+import Contact from "./components/contact";
 import "./globals.css";
 
 export default function Home() {
@@ -17,12 +18,7 @@ export default function Home() {
               </a>
             </li>
             <li className="mx-8">
-              <a className="text-2xl" href="/projets">
-                Projets
-              </a>
-            </li>
-            <li className="mx-8">
-              <a className="text-2xl" href="/competences">
+              <a className="text-2xl" href="skill">
                 Compétences
               </a>
             </li>
@@ -35,34 +31,191 @@ export default function Home() {
           <Burger />
         </nav>
       </header>
-      <section className="w-full h-full">
-        <article className="w-1/2 absolute top-1/3 left-48">
+      <section className="relative flex max-w-screen-xl m-auto min-h-screen ">
+        <article className="relative top-48 w-2/3">
           <p className="mb-4">
             <span className="text-9xl font-bold">Hello !</span>
           </p>
           <p className="text-4xl ">
-            Je suis <span className="text-6xl font-bold">Ulrich</span> <br />
+            Je suis <span className="text-6xl font-bold">Ulrich Vallaud</span>{" "}
+            <br />
             Développeur Web Fullstack Javascript
           </p>
-          <p className="text-2xl mt-4 font-medium">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-            lorem erat, rutrum ac eros in, imperdiet eleifend leo. Nullam id
-            iaculis mauris. Etiam nunc ipsum, suscipit eget sapien vitae,
-            tristique convallis est.
+          <p className="text-2xl mt-4 font-medium w-4/5 justify-text text-wrap">
+            Passionné par la création d&apos;expériences en ligne en partant du
+            site vitrine aux applications complexes.
           </p>
         </article>
-        <div className="border-photo w-96 h-96  absolute top-1/4 right-48">
-          <Image
-            src="/images/avatar.jpg"
-            alt="Photo de Ulrich"
-            width={400}
-            height={400}
-            className="rounded-full"
-          />
-        </div>
+        <article className="w-1/3">
+          <div className="border-photo relative top-1/4 left-1/4">
+            <Image
+              src="/images/avatar.jpg"
+              alt="Photo de Ulrich"
+              width={250}
+              height={250}
+              className="rounded-full"
+            />
+          </div>
+          <div className="flex justify-around relative top-1/3 left-6">
+            <a
+              className="logo text-sm px-4 border-2 border-black rounded-full "
+              href="https://github.com/VALLAUD-Ulrich"
+            >
+              <Image
+                src="/images/github-black.svg"
+                alt="logo github"
+                width={50}
+                height={50}
+              />
+            </a>
+            <a
+              className="logo text-sm px-4 border-2 border-black rounded-full"
+              href="https://www.linkedin.com/in/ulrich-vallaud-18b41124b/"
+            >
+              <Image
+                src="/images/linkedin.svg"
+                alt="logo linkedin"
+                width={50}
+                height={50}
+              />
+            </a>
+            <a
+              className="text-sm logo text-sm px-4 border-2 border-black rounded-full"
+              href="mailto:ulrichvallaud@digitaluniverse.fr"
+            >
+              <Image
+                src="/images/gmail.svg"
+                alt="logo gmail"
+                width={50}
+                height={50}
+              />
+            </a>
+          </div>
+        </article>
       </section>
-      <section></section>
-      <section></section>
+      <section id="skill">
+        <h1 className="text-center text-4xl mb-10 h-">Compétences</h1>
+        <article className="flex justify-evenly">
+          <div className="flex flex-col ">
+            <h3 className="text-center text-2xl ">Design</h3>
+            <div className="flex mt-5 items-center">
+              <Image
+                src="/images/figma.svg"
+                alt="logo figma"
+                width={45}
+                height={45}
+                className="mr-5"
+              />
+              <p className="text-xl">Figma</p>
+            </div>
+            <div className="flex mt-5 items-center">
+              <Image
+                src="/images/mockflow.svg"
+                alt="logo mockflow"
+                width={45}
+                height={45}
+                className="mr-5"
+              />
+              <p className="text-xl">Mockflow</p>
+            </div>
+            <div className="flex mt-5 items-center">
+              <Image
+                src="/images/tailwind.svg"
+                alt="logo tailwind"
+                width={45}
+                height={45}
+                className="mr-5"
+              />
+              <p className="text-xl">tailwind</p>
+            </div>
+          </div>
+          <div>
+            <h3 className="text-center text-2xl">Frontend</h3>
+            <div className="flex mt-5 items-center">
+              <Image
+                src="/images/react.svg"
+                alt="logo react"
+                width={45}
+                height={45}
+                className="mr-5"
+              />
+              <p className="text-xl">React</p>
+            </div>
+            <div className="flex mt-5 items-center">
+              <Image
+                src="/images/nextjs.svg"
+                alt="logo nextjs"
+                width={45}
+                height={45}
+                className="mr-5"
+              />
+              <p className="text-xl">Next.js</p>
+            </div>
+            <div className="flex mt-5 items-center">
+              <Image
+                src="/images/vue.svg"
+                alt="logo vuejs"
+                width={45}
+                height={45}
+                className="mr-5"
+              />
+              <p className="text-xl">Vue.js</p>
+            </div>
+          </div>
+          <div>
+            <h3 className="text-center text-2xl">Backend</h3>
+            <div>
+              <div className="flex mt-5 items-center">
+                <Image
+                  src="/images/nodejs.svg"
+                  alt="logo nodejs"
+                  width={45}
+                  height={45}
+                  className="mr-5 bg-white"
+                />
+                <p className="text-xl">Node.js</p>
+              </div>
+              <div className="flex mt-5 items-center">
+                <Image
+                  src="/images/postgresql.svg"
+                  alt="logo postgresql"
+                  width={45}
+                  height={45}
+                  className="mr-5"
+                />
+                <p className="text-xl">PostgreSQL</p>
+              </div>
+              <div className="flex mt-5 items-center">
+                <Image
+                  src="/images/sequelize.svg"
+                  alt="logo sequelize"
+                  width={45}
+                  height={45}
+                  className="mr-5"
+                />
+                <p className="text-xl">Sequelize</p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <h3 className="text-center text-2xl">Outils de développement</h3>
+            <div className="flex mt-5 items-center">
+              <Image
+                src="/images/github.svg"
+                alt="logo git-github"
+                width={45}
+                height={45}
+                className="mr-5"
+              />
+              <p className="text-xl">Git & GitHub</p>
+            </div>
+          </div>
+        </article>
+      </section>
+      <section id="contact">
+        <h1 className="text-center text-4xl mt-20 mb-20">Me Contacter</h1>
+        <Contact />
+      </section>
       <footer></footer>
     </main>
   );
